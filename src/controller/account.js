7eb982b6b9b4855f86f3f60461b32032af8403a0
Standley Eugene <estand25@gmail.com>
2017-07-23 18:10:09 -0400

    Update log-out route
    Log-out route was not performing correctly. Updated to work correctly.

--- src/controller/account.js
+++ src/controller/account.js
@@ -39,7 +39,7 @@ export default ({config, db}) => {
 
   // 'v1/account/logout'
   api.get('/logout', authenticate, (req,res) =>{
-    res.logout();
+    req.logout();
     res.status(200).send('Successfully logged out');
   });
 
